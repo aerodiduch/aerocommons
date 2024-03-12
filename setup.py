7026b79634d55e7e9b630b92bf1a854c2bebd20c
Federico Perez Diduch <fperezdiduch@hotmail.com>
2024-03-12 16:38:48 -0300

    initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="aerocommons",
+    version="0.0.1",
+    packages=find_packages(),
+    author="Federico Perez Diduch",
+    author_email="fperezdiduch@gmail.com",
+    description="Common utilities for Aerobot",
+    long_description="",
+    url="https://github.com/aerodiduch/aerocommons",
+    python_requires=">=3.6",
+)
