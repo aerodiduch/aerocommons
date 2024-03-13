25306cbce083f325c8f57cc39d5d3ed10220d9d7
Federico Perez Diduch <fperezdiduch@hotmail.com>
2024-03-12 21:33:27 -0300

    feat: updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="aerocommons",
-    version="0.0.1",
+    version="0.0.2",
     packages=find_packages(),
     author="Federico Perez Diduch",
     author_email="fperezdiduch@gmail.com",
