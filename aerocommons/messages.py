065fc354ae4914495e3b664b39329fb4b7be7f02
Federico Perez Diduch <fperezdiduch@hotmail.com>
2024-03-13 21:10:21 -0300

    feat: added default messages

---
+++ aerocommons/messages.py
@@ -0,0 +1,4 @@
+DEFAULT_MESSAGES = {
+    "error_message": "🤖 Bzz! Hubo un problema procesando tu mensaje, ya estoy investigando que pasó 🐒 🔧",
+    "not_premium": "🤖 Bzz! Temporalmente, *debido a la situación económica*, Aerobot *estará disponible solo para usuarios suscriptos*. \nSi queres *seguir usando aerobot como hasta ahora*, podes suscribrite por *$1500/mes* 👉🏻 https://aerobot.com.ar/apoyar.\n\nSi queres conocer más acerca de esta decisión 👉🏻 https://aerobot.com.ar/comunicado ",
+}
