245867a70d15c6f384eab57d30c3fa7c6776b250
Federico Perez Diduch <fperezdiduch@hotmail.com>
2026-05-21 23:36:02 -0300

    feat: added get_logger with error notifier handler

---
+++ aerocommons/logger.py
@@ -0,0 +1,46 @@
+import logging
+import requests
+
+
+def _send_to_notifier(record_name: str, message: str):
+    try:
+        requests.post(
+            "http://aerobot-error-notifier:60615/usageerror",
+            json={
+                "number": None,
+                "intent": record_name,
+                "traceback": message,
+                "additional_data": None,
+            },
+            timeout=3,
+        )
+    except Exception:
+        pass
+
+
+class _ErrorNotifierHandler(logging.Handler):
+    def emit(self, record):
+        _send_to_notifier(record.name, self.format(record))
+
+
+def get_logger(name: str) -> logging.Logger:
+    logger = logging.getLogger(name)
+    if logger.handlers:
+        return logger
+
+    formatter = logging.Formatter(
+        "%(asctime)s [%(levelname)s] %(name)s: %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(formatter)
+    logger.addHandler(stream_handler)
+
+    error_handler = _ErrorNotifierHandler(level=logging.ERROR)
+    error_handler.setFormatter(formatter)
+    logger.addHandler(error_handler)
+
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    return logger
